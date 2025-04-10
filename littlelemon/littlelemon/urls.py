2b212253434cd4f70e48a452af361497f148e03c
Keerthika-G <garimellakeerthika@gmail.com>
2025-04-10 18:47:24 -0500

    url change

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.db import router
 from django.urls import path, include
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include("restaurant.urls")),
